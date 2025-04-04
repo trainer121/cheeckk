b355b1201987f6bd9e4ec1d40cab29b305985563
FIRST_NAME LAST_NAME <dineshreddy1590@gmail.com>
2025-04-04 13:58:35 +0530

    cc

---
+++ cheeckk/src/main/java/com/example/cheeckk/CheeckkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheeckk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeckkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeckkApplication.class, args);
+	}
+
+}
---
+++ cheeckk/src/test/java/com/example/cheeckk/CheeckkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheeckk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeckkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
